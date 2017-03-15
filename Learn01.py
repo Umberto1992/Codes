30bb7612e2f68e4dec1fe9d1c87495c556727ab8
Umberto1992 <umberto_bollo@libero.it>
2017-03-15 15:09:02 +0100

    updatearr
    The end of testing arrays

--- Learn01.py
+++ Learn01.py
@@ -1,13 +1,14 @@
 
 # -*- coding: utf-8 -*-
-#  #
-#  #
+#  # This is code was helping me to understand how to use
+#  # arrays.
 # Umberto Bollo                              #
-#"""
-#Created on Wed Mar 15 12:05:15 2017
-#
-#@author: Umberto
-#"""
+"""
+Created on Wed Mar 15 12:05:15 2017
 
-xs = [3, 2, 4]
+@author: Umberto
+"""
+
+xs = [3, 5, 4]
 xs[0] = xs[1] + xs[2]
+
