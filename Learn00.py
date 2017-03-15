57f57c41474be6e3ce0782d90cf190f111500bf2
Umberto1992 <umberto_bollo@libero.it>
2017-03-15 12:20:05 +0100

    First Version
    4+2

---
+++ Learn00.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# First program to try how to code in phyton #
+# Use of integers, sum and print             #
+# Umberto Bollo
+c = 4
+
+
+e = c + d
+
+print ("The result is ", e)
+
+     
