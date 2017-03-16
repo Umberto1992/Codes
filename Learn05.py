d60413d6f0175a6a4ba3c5a36c5002f2f35193d1
Umberto1992 <umberto_bollo@libero.it>
2017-03-16 13:41:00 +0100

    Mean and Variance

---
+++ Learn05.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# test of mean and variance
+#
+#
+"""
+Created on Thu Mar 16 13:33:25 2017
+
+@author: Umberto
+"""
+
+import numpy
+k = numpy.array( ((3,5), (2,9), (3,7)) )
+
+meank = numpy.mean(k)
+vark = numpy.var(k)
