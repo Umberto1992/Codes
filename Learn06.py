1bdec23af871a25a9a17e4e0877187d44bb0d1f3
Umberto1992 <umberto_bollo@libero.it>
2017-03-17 16:47:31 +0100

    Dataset created
    Created a dataset to implement a learning algorithm

--- Learn06.py
+++ Learn06.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Use of random function and create a dataset and plot it
+#
+
 """
 Created on Fri Mar 17 14:38:00 2017
 
@@ -11,7 +14,7 @@ import matplotlib.pyplot as plt
 
 np.random.seed(12)
 
-X, y = sklearn.datasets.make_moons(50, noise=0.10)
+X, y = sklearn.datasets.make_moons(50, noise=0.001)
 plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
 
 print(X)
---
+++ Learn07.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#Creation of a dateset to implement a classifier
+"""
+Created on Fri Mar 17 14:52:20 2017
+
+@author: Umberto
+"""
+
+import numpy as np
+import sklearn.datasets
+import matplotlib.pyplot as plt
+
+np.random.seed(12)
+
+iterations = 50
+
+X1 = np.zeros((2*iterations,2))
+y = []
+
+loop = range(0,iterations)
+
+for i in loop:
+    X1[i] = np.random.normal(2,0.9,2)
+    y.append(1)
+    
+for i in loop:
+    X1[i + iterations] = np.random.normal(-2,0.9,2)
+    y.append(-1)
+    
+plt.scatter(X1[:,0], X1[:,1], s=40, c=y, cmap=plt.cm.Spectral)
