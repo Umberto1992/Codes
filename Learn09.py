5176c1a218ca572dbf6ebf5660c2f9228dd60edf
Umberto1992 <umberto_bollo@libero.it>
2017-03-21 10:49:05 +0100

    Update of the feed-forward
    Update of the feed-forward. I put just one output, that will be 1 for
    class 1 and 0 for class 0

---
+++ Learn09.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 10:18:04 2017
+
+@author: Umberto
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(12)
+
+iterations = 50
+
+X1 = np.zeros((2*iterations,2))
+y = []
+
+loop = range(0,iterations)
+
+for i in loop:
+    X1[i] = np.random.normal(2,0.9,2)
+    y.append(1)
+    
+for i in loop:
+    X1[i + iterations] = np.random.normal(-2,0.9,2)
+    y.append(0)
+    
+plt.scatter(X1[:,0], X1[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+
+###############################################################
+
+W1 = np.random.rand(3,2)
+W2 = np.random.rand(1,3)
+b1 = np.random.rand(1,3)
+b2 = np.random.rand()
+out = np.zeros((2*iterations))
+
+# forward-pass of a 2-layer neural network:
+f = lambda x: 1.0/(1.0 + np.exp(-x)) # activation function (use sigmoid)
+for i in loop:
+     x = X1[i] # random input vector of two numbers (2x1)
+     h1 = f(np.dot(W1, x) + b1) # calculate first hidden layer activations (2x1)
+     out[i] = np.dot(W2, np.transpose(h1)) + b2 # output neuron (2x1)
+     
+for i in loop:
+     x = X1[iterations + i] # random input vector of two numbers (2x1)
+     h1 = f(np.dot(W1, x) + b1) # calculate first hidden layer activations (2x1)
+     out[iterations + i] = np.transpose(np.dot(W2, np.transpose(h1)) + b2) # output neuron (2x1)
+
